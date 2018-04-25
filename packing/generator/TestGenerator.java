aa2d0165d4a8351cf9daeb6fefcf030cca84ccd3
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-04-25 13:36:27 +0200

    Added class

---
+++ packing/generator/TestGenerator.java
@@ -0,0 +1,16 @@
+
+package packing.generator;
+
+
+// Packing imports
+import packing.data.Dataset;
+
+
+public class TestGenerator extends Generator {
+    
+    @Override
+    public Dataset generate(Dataset dataset) {
+        return dataset;
+    }
+    
+}
