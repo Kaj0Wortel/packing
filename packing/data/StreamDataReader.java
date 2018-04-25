b0cd5a38c2470b7e1c45b8c9e7f3b52f9efd8d3a
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-04-25 13:36:48 +0200

    Added class

---
+++ packing/data/StreamDataReader.java
@@ -0,0 +1,44 @@
+
+package packing.data;
+
+// Packing imports
+import packing.data.Dataset;
+
+
+// Java imports
+import java.io.InputStream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+/* 
+ * Reads data from a stream and provides a Dataset for this data.
+ */
+public class StreamDataReader
+        extends AbstractReader {
+    final private InputStream stream;
+    
+    
+    public StreamDataReader(InputStream stream, OutputWriter ow) {
+        super(ow);
+        this.stream = stream;
+    }
+    
+    
+    @Override
+    public Dataset readEntries() {
+        Scanner sc = new Scanner(stream);
+        
+        List<String> data = new ArrayList<String>();
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            data.add(line);
+            super.outputLine(line);
+        }
+        
+        return DatasetFactory.process(data);
+    }
+    
+}
