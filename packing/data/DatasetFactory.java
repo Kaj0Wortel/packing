b35b882c7c5a5a3c02c55442777df6db2c44ec72
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-04-25 13:36:08 +0200

    Created  class

---
+++ packing/data/DatasetFactory.java
@@ -0,0 +1,92 @@
+
+package packing.data;
+
+
+// Packing imports
+import packing.generator.*;
+
+
+// Java imports
+import java.awt.Rectangle;
+
+import java.util.List;
+
+
+public class DatasetFactory {
+    
+    
+    public static Dataset process(List<String> input) {
+        int height;
+        boolean rotation;
+        int numRect;
+        
+        // Determine the height
+        String line = input.get(0);
+        if (line.endsWith("free")) {
+            height = -1;
+            
+        } else {
+            int loc = line.length();
+            while (line.charAt(--loc) != ' ') { }
+            
+            String number = line.substring(loc + 1, line.length());
+            
+            try {
+                height = Integer.parseInt(number);
+                
+            } catch (NumberFormatException e) {
+                throw new IllegalStateException("Illegal input");
+            }
+        }
+        
+        // Determine whether rotations are allowed or not.
+        line = input.get(1);
+        rotation = line.endsWith("yes");
+        
+        // Determine the number of rectangles.
+        line = input.get(2);
+        int loc = line.length();
+        while (line.charAt(--loc) != ' ') { }
+        
+        String number = line.substring(loc + 1, line.length());
+        
+        try {
+            numRect = Integer.parseInt(number);
+            
+        } catch (NumberFormatException e) {
+            throw new IllegalStateException("Illegal input: " + line);
+        }
+        
+        // Create the dataset.
+        Dataset dataset = createDataset(height, rotation, numRect, input);
+        
+        // Fill the dataset.
+        for (int i = 3; i < input.size(); i++) {
+            String[] coords = input.get(i).split(" ");
+            
+            try {
+                int recWidth  = Integer.parseInt(coords[0]);
+                int recHeight = Integer.parseInt(coords[1]);
+                dataset.add(new Rectangle(recWidth, recHeight));
+                
+            } catch (NumberFormatException e) {
+                throw new IllegalStateException("Illegal input: " + line);
+            }
+        }
+        
+        return dataset;
+    }
+    
+    
+    private static Dataset createDataset(int height, boolean rotation,
+                                         int numRect, List<String> input) {
+        if (true) {// todo
+            return new TestDataset(height, rotation, numRect,
+                                   new TestGenerator());
+        }
+        
+        return null;
+    }
+    
+    
+}
