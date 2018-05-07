76f4837a71f07a5a734257db193870062bbb8899
Marten <marten.knbk@gmail.com>
2018-05-07 16:05:45 +0200

    Changed variable name.

--- packing/src/PerfectPackingTransformer.java
+++ packing/src/PerfectPackingTransformer.java
@@ -5,16 +5,18 @@
  * Used in the absolute placement approach.
  */
 public class PerfectPackingTransformer extends Packer {
-    private Packer yPacker;
+    private Packer wrappedPacker;
 
     public PerfectPackingTransformer(Packer packer) {
-        this.yPacker = packer;
+        this.wrappedPacker = packer;
     }
     @Override
     public Dataset pack(Dataset dataset) {
         /*
         Clone the dataset, then add 1x1 rectangles until the total area
         of the rectangles is equal to the total are of the bounding box.
+
+        Call wrappedPacker.pack() and return the result.
          */
         return null;
     }
