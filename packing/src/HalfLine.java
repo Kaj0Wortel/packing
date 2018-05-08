e0b30029bce51ce9df0cc2d1f6f49cdbbc50359c
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-05-09 00:34:09 +0200

    Added file

---
+++ packing/src/HalfLine.java
@@ -0,0 +1,89 @@
+
+// Java imports
+import java.awt.Rectangle;
+import java.awt.Point;
+
+
+/**
+ * Class representing a half-line.
+ */
+public class HalfLine
+        implements Cloneable {
+    public static enum Direction {
+        UP, RIGHT, DOWN, LEFT;
+    }
+    
+    public int x;
+    public int y;
+    public Direction dir;
+    
+    /**
+     * Create a half-line starting at [@code (x, y)} in the direction of
+     * {@code dir}.
+     * 
+     * @param x1 the start x-coord of the line.
+     * @param y1 the start y-coord of the line.
+     * @param dir the direction of the line.
+     * @throws NullPointerException iff {@code dir == null}.
+     */
+    public HalfLine(int x, int y, Direction dir) {
+        if (dir == null) throw new NullPointerException("Invallid direction!");
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+    
+    /**
+     * Checks if the line intersects with the bounderies of the rectangle.
+     * 
+     * @param rec the rectangle to check for.
+     * @return whether the line intersects {@code rec}.
+     * 
+     * Note that the line always intersects if it starts within or at an edge
+     * (including the corners) of {@code rec}.
+     */
+    public boolean intersects(Rectangle rec) {
+        switch (dir) {
+            case UP:
+                return (rec.x <= x && x <= rec.x + rec.width) &&
+                        (y <= rec.y + rec.height);
+            case RIGHT:
+                return (rec.y <= y && y <= rec.y + rec.height) &&
+                        (x <= rec.x + rec.width);
+            case DOWN:
+                return (rec.x <= x && x <= rec.x + rec.width) &&
+                        (y >= rec.y);
+            case LEFT:
+                return (rec.y <= y && y <= rec.y + rec.height) &&
+                        (x >= rec.x);
+            default:
+                return false;
+        }
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "[P:(" + x + ", " + y + "), D:" + dir.toString() + "]";
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof HalfLine)) return false;
+        HalfLine line = (HalfLine) obj;
+        return dir == line.dir &&
+                 x ==   line.x &&
+                 y ==   line.y;
+    }
+    
+    @Override
+    public int hashCode() {
+        return MultiTool.calcHashCode(x, y, dir);
+    }
+    
+    @Override
+    public HalfLine clone() {
+        return new HalfLine(x, y, dir);
+    }
+    
+}
