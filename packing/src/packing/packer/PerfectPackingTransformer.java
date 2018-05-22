9d997a4d8ab86161fdcee0902ec0e32d58599d8c
ThomasDerwig <Thomas_Derwig@outlook.com>
2018-05-22 09:54:08 +0200

    implemented perfectPackingTransformer.
    Note: I made it such that the 1x1 boxes get an x-coordinate already
    determined, similar to the input rectangles. This probably makes
    y-packing easier

--- packing/src/packing/packer/PerfectPackingTransformer.java
+++ packing/src/packing/packer/PerfectPackingTransformer.java
@@ -3,6 +3,7 @@ package packing.packer;
 
 
 // Packing imports
+import java.awt.Rectangle;
 import packing.data.*;
 
 
@@ -26,6 +27,38 @@ public class PerfectPackingTransformer extends Packer {
 
         Call wrappedPacker.pack() and return the result.
          */
-        return null;
+        int rectArea = 0;
+        int area = 0;
+        int[] columns = new int[dataset.getWidth()-1]; // columns[0] being the colums from x = 0 to x = 1
+        
+        area = dataset.getArea();
+        Dataset perfectDataSet = dataset.clone();
+        for(Dataset.Entry entry: dataset){
+            Rectangle rec = entry.getRec();
+            for(int i = rec.x; i <(rec.x + rec.width -1); i++){
+                columns[i] += rec.height;
+            }
+            rectArea += entry.area();
+        }
+       
+       /*
+        adds 1x1 rectangles to every column whose total height is not yet the
+        height of the bounding box. Doing this for every column is equivalent 
+        to adding 1x1 rectangles until total rectangle area = total area of bounding box.
+        Every rectangle has a fixed x coordinate this way which means we do not 
+        need to distinguish between fixed x and not when placing the y-coordinates
+        */
+       for(int i : columns){
+            while(columns[i] < dataset.getHeight()){
+                Rectangle rec = new Rectangle(i,0,1,1);
+                perfectDataSet.add(rec);
+                columns[i]++;
+            }
+        }
+        
+        
+        Dataset wrappedDataSet = wrappedPacker.pack(perfectDataSet);
+        
+        return wrappedDataSet;
     }
 }
