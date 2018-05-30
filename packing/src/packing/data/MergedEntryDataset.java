b48055446865c3fd4ccabf2f19e6ccf56226b50e
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-05-30 10:26:39 +0200

    merge

--- packing/src/packing/data/MergedEntryDataset.java
+++ packing/src/packing/data/MergedEntryDataset.java
@@ -1,8 +1,13 @@
+
 package packing.data;
 
+
+// Java imports
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 
 /**
  * This class represents a merged {@link packing.data.CompareEntry}.
@@ -17,31 +22,59 @@ public class MergedEntryDataset
             extends Dataset.Entry {
         final List<CompareEntry> entries;
         
+        
+        /**
+         * Default constructor.
+         * 
+         * @param entries the entries to be merged.
+         * @param id 
+         */
         protected MergedEntry(List<CompareEntry> entries, int id) {
-            super(new Rectangle(Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 0),
-                    id);
+            super(new Rectangle
+                (Integer.MAX_VALUE, Integer.MAX_VALUE, 0, 0), id);
             this.entries = entries;
             
             for (CompareEntry entry : entries) {
                 Rectangle main = entry.getRec();
                 entry.rotate();
-                Rectangle rotated = entry.getNormalRec();
+                Rectangle rotated = entry.getRec();
                 entry.rotate();
                 
                 rotated.x = main.y;
                 rotated.y = main.x;
                 
                 // Update the current bounding rectangle.
-                if (main.x < rec.x)
-                    rec.x = rotated.x;
-                if (main.y < rec.y)
-                    rec.y = rotated.y;
+                if (main.x < rec.x) {
+                    rec.width += rec.x - main.x;
+                    rec.x = main.x;
+                }
+                
+                if (main.y < rec.y) {
+                    rec.height += rec.y - main.y;
+                    rec.y = main.y;
+                }
+                
                 if (main.x + main.width > rec.x + rec.width)
                     rec.width = main.x + main.width - rec.x;
                 if (main.y + main.height > rec.y + rec.height)
                     rec.height = main.y + main.height - rec.y;
             }
         }
+        
+        /**
+         * Clone constructor.
+         * 
+         * @param clone 
+         */
+        protected MergedEntry(MergedEntry clone) {
+            super((Rectangle) clone.rec.clone(), clone.id);
+            entries = new ArrayList<CompareEntry>();
+            
+            for (CompareEntry entry : clone.entries) {
+                entries.add(entry.clone());
+            }
+        }
+        
         
         @Override
         public void setRotation(boolean rotation) {
@@ -63,6 +96,19 @@ public class MergedEntryDataset
             }
         }
         
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof MergedEntry)) return false;
+            MergedEntry entry = (MergedEntry) obj;
+            
+            return entries == entry.entries && super.equals(entry);
+        }
+        
+        @Override
+        public MergedEntry clone() {
+            return new MergedEntry(this);
+        }
+        
     }
     
     
@@ -81,7 +127,7 @@ public class MergedEntryDataset
             list.add(entry);
         }
         
-        idCounter += list.size();
+        idCounter = dataset.idCounter;
     }
     
     
@@ -115,15 +161,18 @@ public class MergedEntryDataset
      * of the new entry however are delegated to the old entries, which are
      * changed in the initial dataset.
      * 
-     * @param entryNums the entries to be merged.
+     * @param entryIndices Nums the entries to be merged.
      */
     public void mergeEntries(int... entryIndices) {
         List<CompareEntry> entries = new ArrayList<CompareEntry>();
         
-        for (int i : entryIndices) {
-            entries.add(list.get(i));
-            list.remove((int) i);
-        }
+        // Iterate entryIndices in reverse sorted order, so the indices
+        // of the entries we still have to remove don't shift before
+        // we can remove them.
+        Arrays.sort(entryIndices);
+        for (int i = entryIndices.length - 1; i >= 0; i--) {
+            entries.add(list.remove(entryIndices[i]));
+         }
         
         list.add(new MergedEntry(entries, idCounter++));
     }
