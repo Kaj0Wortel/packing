822614b7578fedf06347e67499212d114b425981
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-05-31 16:57:23 +0200

    Completed class

--- packing/src/packing/io/CanvasSubmission.java
+++ packing/src/packing/io/CanvasSubmission.java
@@ -8,10 +8,13 @@ import packing.tools.MultiTool;
 
 //##########
 // Java imports
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import java.io.PrintWriter;
 import java.util.List;
 
 
@@ -38,12 +41,39 @@ public class CanvasSubmission {
                 .listFilesAndPathsFromRootDir(new File(SOURCE));
         
         for (File[] file : files) {
-            try {
-                if (file[0].isDirectory()) continue;
-                Files.copy(file[0].toPath(),
-                        new File(DEST + file[0].getName()).toPath(),
-                        StandardCopyOption.REPLACE_EXISTING);
-                
+            File source = file[0];
+            if (source.isDirectory()) continue;
+            File dest = new File(DEST + file[0].getName());
+            dest.delete();
+            
+            try (BufferedReader br
+                    = new BufferedReader(new FileReader(source))) {
+                //try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dest)))) {
+                try (PrintWriter pw = new PrintWriter(
+                        new BufferedWriter(new FileWriter(dest, false)))) {
+                    
+                    String line = br.readLine();
+                    if (line == null) continue;
+                    boolean hasHeader = line.startsWith("/* * * * * * *");
+                    if (hasHeader) {
+                        pw.println(line);
+                    }
+                    
+                    while ((line = br.readLine()) != null) {
+                        if (line.equals("//##########")) break;
+                        if (hasHeader) {
+                            pw.println(line);
+                        }
+                        if (line.endsWith("* * * */")) hasHeader = false;
+                    }
+                    
+                    while ((line = br.readLine()) != null) {
+                        pw.println(line.replaceAll("packing.tools.Cloneable", "Cloneable"));
+                    }
+                    
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
