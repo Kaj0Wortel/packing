58eb9785d2af80c00b3309e9bdc0435feb563559
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-06-10 12:58:49 +0200

    Implemented polish notation stuff

--- packing/src/packing/data/PolishDataset.java
+++ packing/src/packing/data/PolishDataset.java
@@ -7,31 +7,68 @@ package packing.data;
 import java.awt.Rectangle;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Random;
 
 
 /**
- * 
+ * Dataset that stores the placement of the entries in a polish notation.
  */
 public class PolishDataset
         extends DatasetDecorator {
     
+    // The used random variable.
     final private static Random random = new Random();
     
+    /**
+     * Direction class for denoting the operator direction.
+     * Let A and B be rectangles, then:
+     * A-B-UP means that B is above A, and 
+     * A-B-RIGHT means that B on the right of A.
+     */
     public static enum Direction {
         RIGHT, UP;
     }
     
+    
+    /**
+     * Operator class.
+     * This class denotes the type of operator used in the polish notation.
+     */
     public class Operator
             extends Dataset.Entry {
         final private Direction dir;
         
+        /**
+         * Creates a new operator with the provided direction.
+         * 
+         * @param dir the direcion of the operator.
+         */
         public Operator(Direction dir) {
             super(null, -1);
             this.dir = dir;
         }
         
+        /**
+         * Creates a new operator using the provided boolean option as
+         * operator.
+         * 
+         * @param option converted to {@link Direction#RIGHT} if true,
+         *     {@link Direction#UP} otherwise.
+         */
+        public Operator(boolean option) {
+            this(option ? Direction.RIGHT : Direction.UP);
+        }
+        
+        /**
+         * Randomly creates a new operator.
+         */
+        public Operator() {
+            this(random.nextBoolean());
+        }
+        
+        /**
+         * @return the direction of the operator.
+         */
         public Direction getDirection() {
             return dir;
         }
@@ -45,17 +82,32 @@ public class PolishDataset
         
         @Override
         public String toString() {
-            return (dir == Direction.UP ? "+" : "*");
+            return "[" + dir.toString() + "]";
         }
         
     }
     
+    
+    /**
+     * Creates a new PolishDataset from the provided dataset.
+     * 
+     * @param dataset the input dataset.
+     */
     public PolishDataset(Dataset dataset) {
         super(dataset);
         list = new LinkedList<CompareEntry>();
     }
     
-    public PolishDataset(Dataset dataset, List<CompareEntry> list) {
+    /**
+     * Creates a new PolishDataset from the provided dataset using the
+     * provided list.
+     * 
+     * @param dataset the input dataset.
+     * @param list the list to be used.
+     * 
+     * Note that the list is not cloned.
+     */
+    public PolishDataset(Dataset dataset, LinkedList<CompareEntry> list) {
         super(dataset);
         this.list = list;
     }
@@ -67,6 +119,8 @@ public class PolishDataset
     
     /**
      * Initializes the dataset with a random reverse polish notation.
+     * Note that the elements still occur in the same order as in
+     * the provided dataset.
      */
     public void init() {
         list.clear();
@@ -82,16 +136,7 @@ public class PolishDataset
                     (!it.hasNext() || random.nextBoolean())) {
                 // Add an operator.
                 numOp++;
-                Operator op;
-                if (random.nextBoolean()) {
-                    op = new Operator(Direction.UP);
-                    //list.add(new Operator(Direction.UP));
-                } else {
-                    op = new Operator(Direction.RIGHT);
-                    //list.add(new Operator(Direction.RIGHT));
-                }
-                System.err.println(op);
-                list.add(op);
+                list.add(new Operator());
                 
             } else {
                 // Add an element.
@@ -104,33 +149,57 @@ public class PolishDataset
         while(numOp < size - 1) {
             // Select an operator.
             numOp++;
-            Operator op;
-            if (random.nextBoolean()) {
-                op = new Operator(Direction.UP);
-                //list.add(new Operator(Direction.UP));
-            } else {
-                op = new Operator(Direction.RIGHT);
-                //list.add(new Operator(Direction.RIGHT));
-            }
-            System.err.println(op);
-            list.add(op);
+            list.add(new Operator());
         }
     }
     
+    /**
+     * Set the default iterator to the iterator of the input dataset since
+     * in this way the additional operators are ignored, and when not yet
+     * initialized there is still is a solution available.
+     */
+    @Override
+    public Iterator<CompareEntry> iterator() {
+        return dataset.iterator();
+    }
+    
+    /**
+     * @return an iterator to iterate over the elements in the list.
+     */
+    public Iterator<CompareEntry> listIterator() {
+        return list.iterator();
+    }
     
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
         String[] strs = new String[list.size()];
-        //strs[0] =getClass().getSimpleName() + "["
         
-        Iterator<CompareEntry> it = list.iterator();
         int i = 0;
-        while (it.hasNext()) {
-            strs[i++] = it.next().toString();
+        for (CompareEntry entry : list) {
+            strs[i++] = entry.toString();
         }
         
         return getClass().getSimpleName() + "[" + String.join("", strs) + "]";
+    }
+    
+    /**
+     * @return a short representation of the data using id's as identifiers.
+     */
+    public String toShortString() {
+        String[] strs = new String[list.size()];
+        
+        int i = 0;
+        for (CompareEntry entry : list) {
+            if (entry instanceof Operator) {
+                strs[i++] = entry.toString();
+            } else {
+                strs[i++] = "[" + entry.id + "]";
+            }
+        }
+        
+        return getClass().getSimpleName() + "[" + "width=" + width
+                + ", height=" + height + ", elems:[" + String.join("", strs)
+                + "]]";
     }
     
     public static void main(String[] args) {
--- packing/src/packing/packer/PolishPacker.java
+++ packing/src/packing/packer/PolishPacker.java
@@ -1,26 +1,30 @@
 
 package packing.packer;
 
+
 // Packing imports
+import packing.data.CompareEntry;
 import packing.data.Dataset;
+import packing.data.MergedEntryDataset;
+import packing.data.MergedEntryDataset.MergedEntry;
+import packing.data.PolishDataset;
+import packing.data.PolishDataset.Direction;
+import packing.data.PolishDataset.Operator;
+import packing.gui.ShowDataset;
+import packing.tools.MultiTool;
+
 
 //##########
 // Java imports
-import java.util.LinkedList;
-import java.util.List;
+import java.awt.Rectangle;
+import java.util.Iterator;
+import java.util.Stack;
 
 
 /**
  *
  */
 public class PolishPacker extends Packer {
-    
-    protected List<Elem> list = new LinkedList<>();
-    
-    protected interface Elem {
-        
-    }
-    
     
     
     public PolishPacker() {
@@ -29,8 +33,81 @@ public class PolishPacker extends Packer {
     
     
     @Override
-    public Dataset pack(Dataset dataset) {
-        return null;
+    public PolishDataset pack(Dataset dataset) {
+        PolishDataset pd;
+        if (dataset instanceof PolishDataset) {
+            pd = (PolishDataset) dataset;
+            
+        } else {
+            dataset.shuffle();
+            pd = new PolishDataset(dataset);
+            pd.init();
+        }
+        
+        MergedEntryDataset med = new MergedEntryDataset(dataset); // TODO
+        
+        Iterator<CompareEntry> it = pd.listIterator();
+        Stack<CompareEntry> entryStack = new Stack<>();
+        while (it.hasNext()) {
+            CompareEntry entry = it.next();
+            if (entry instanceof Operator) {
+                // If the entry is an operator, merge the last two elements.
+                Operator op = (Operator) entry;
+                CompareEntry e2 = entryStack.pop();
+                CompareEntry e1 = entryStack.pop();
+                
+                Rectangle rec = e1.getRec();
+                if (op.getDirection() == Direction.UP) {
+                    e2.setLocation(rec.x, rec.y + rec.height);
+                    System.err.println("UP");
+                    
+                } else { // dir == Direction.RIGHT.
+                    e2.setLocation(rec.x + rec.width, rec.y);
+                    System.err.println("RIGHT");
+                }
+                
+                // Merge the entries.
+                MergedEntry me = med.merge(e1, e2);
+                
+                // Push the element on the stack.
+                entryStack.push(me);
+                
+            } else {
+                // Push the element on the stack.
+                entryStack.push(entry);
+            }
+        }
+        
+        // Update the bounds of the dataset.
+        // Note that at this point the merged entry dataset has exactly one
+        // entry containing all entries of the dataset.
+        Rectangle meRec = med.iterator().next().getRec();
+        pd.setSize(meRec.width, meRec.height);
+        
+        return pd;
+    }
+    
+    
+    public static void main(String[] args) {
+        Dataset data = new Dataset(-1, true, 5);
+        data.add(1, 1);
+        data.add(2, 2);
+        data.add(3, 3);
+        data.add(4, 4);
+        /*
+        PolishDataset test = new PolishDataset(data);
+        test.init();
+        test.setSize(5, 5);
+        new ShowDataset(test);
+        */
+        /**/
+        PolishDataset result = new PolishPacker().pack(data);
+        MultiTool.sleepThread(1000);
+        System.out.println(result.toShortString());
+        System.out.println("width=" + result.getWidth());
+        System.out.println("height=" + result.getHeight());
+        new ShowDataset(result);
+        /**/
     }
     
     
