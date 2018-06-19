34864e3d342ffa33263a1d487342516766f0432b
CFKaligula <casper.kole@gmail.com>
2018-06-19 18:45:12 +0200

    test  change

--- packing/src/packing/generator/OptimalBoundingBoxGenerator.java
+++ packing/src/packing/generator/OptimalBoundingBoxGenerator.java
@@ -27,7 +27,7 @@ import java.util.*;
 public class OptimalBoundingBoxGenerator extends Generator {
 
     static {
-        name = "optimal";
+        name = "Optimal";
     }
 
     public OptimalBoundingBoxGenerator(PackerFactory factory) {
